003023c9376d5a01ed1378fb6ffd39a9094a97e8
tafu90 <46842792+tafu90@users.noreply.github.com>
2025-05-16 15:45:28 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-/usr/sbin/avahi-daemon --no-chroot &
+/usr/sbin/avahi-daemon --no-chroot --no-dbus --daemonize &
 
 # Fake Synology mDNS broadcast
-avahi-publish -s "Maina" _http._tcp 5000 &
-avahi-publish -s "Maina" _smb._tcp 445 &
+avahi-publish -s "Maina" _http._tcp 5000 --interface=eth0 &
+avahi-publish -s "Maina" _smb._tcp 445 --interface=eth0 &
 
 # Keep container alive
 tail -f /dev/null
